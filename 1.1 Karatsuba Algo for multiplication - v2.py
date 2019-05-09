10727ced49aba4f280dd90162a138f2ee6308403
Krokette29 <hyh19957299573@alumni.sjtu.edu.cn>
2019-05-09 19:58:33 +0200

    karatsuba v2

---
+++ 1.1 Karatsuba Algo for multiplication - v2.py
@@ -0,0 +1,77 @@
+import time
+
+def karatsubaRec(x, y):
+	'''
+	Multiplication using Karatsuba Algorithm.
+	Input two integers, no matter how long they respectivly are.
+	Quite perfect function.
+	'''
+	x = str(x)
+	y = str(y)
+
+	# x, y - string
+
+	# initialize length list of x and y
+	n = [0, 0]
+
+	n[0] = len(x)
+	n[1] = len(y)
+	if n[0] == 1 or n[1] == 1:
+		return int(x) * int(y)
+
+	flag = 0
+	# devide
+	if n[0] < n[1]:
+		nDevide = n[0]//2
+		a = int(x[:nDevide])
+		b = int(x[nDevide:])
+		c = int(y[:(n[1] - n[0] + nDevide)])
+		d = int(y[(n[1] - n[0] + nDevide):])
+	else:
+		# flag is used in 'return'
+		flag = 1
+		nDevide = n[1]//2
+		a = int(x[:(n[0] - n[1] + nDevide)])
+		b = int(x[(n[0] - n[1] + nDevide):])
+		c = int(y[:nDevide])
+		d = int(y[nDevide:])	
+
+	# a, b, c, d - int
+
+	ac = karatsubaRec(a, c)
+	bd = karatsubaRec(b, d)
+
+	gaussNum = karatsubaRec(a+b, c+d) - ac - bd
+
+	return int(zeroPad(str(ac), 2 * (n[0+flag] - nDevide))) + int(zeroPad(str(gaussNum), (n[0+flag] - nDevide))) + bd
+
+
+def zeroPad(numStr, zeroNum):
+	'''
+	Add zeroNum zeros at the end of numStr.
+
+	input - string, int
+	output - string
+	'''
+	for i in range(zeroNum):
+		numStr = numStr + '0'
+
+	return numStr
+
+
+x = input('input1:')
+y = input('input2:')
+
+# start the timer
+time.process_time()
+
+myAnswer = karatsubaRec(x, y)
+exactAnswer = int(x) * int(y)
+print('My answer:' + str(myAnswer))
+print('Exact answer:' + str(exactAnswer))
+
+error = myAnswer - exactAnswer
+print('Error:' + str(error))
+
+# calculate the running time
+print('Running Time: {:.3}s'.format(time.process_time()))
--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-a = [1, 3, 5]
+x = '11'
+y = '111'
 
-print(int(a))
+n1 = len(x)
+n2 = len(y)
+
+nDevide = n1//2
+a = int(x[:nDevide])
+b = int(x[nDevide:])
+c = int(y[:(n2 - n1 + nDevide)])
+d = int(y[(n2 - n1 + nDevide):])
+
+print(a)
+print(b)
+print(c)
+print(d)
+print('nDevide: {}'.format(nDevide))
+print(2//2)
