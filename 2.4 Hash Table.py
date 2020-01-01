c3b4b35369d7bea3b863d427d2bcf5d11e487d78
Yuhan Huang <hyh19957299573@alumni.sjtu.edu.cn>
2020-01-01 01:31:18 +0100

    add hash table using open addressing

--- 2.4 Hash Table.py
+++ 2.4 Hash Table.py
@@ -122,6 +122,147 @@ class HashTableUsingChaining(object):
                         break
 
 
+class HashTableUsingOpenAddressing(object):
+    def __init__(self, length,  key_type="int", mode='l'):
+        self.hash_table = [HashNode(None, None) for i in range(length)]
+        self.length = length
+        self.key_type = key_type
+        self.hash_function_n = self.__max_prime_num(length)
+        self.hash_function_n2 = 5
+
+        if mode == 'l':
+            self.hash_mode = "LinearProbing"
+        elif mode == 'd':
+            self.hash_mode = "DoubleHashing"
+        else:
+            raise ValueError("Unknown hash table type!")
+
+    def __max_prime_num(self, length):
+        """
+        Calculate the largest prime number smaller than the length.
+        Args:
+            length: length of the hash table
+
+        Returns:
+            value: largest prim number smaller than the length
+
+        """
+        for value in reversed(range(1, length + 1)):
+            prime = True
+            for i in range(2, value):
+                if value % i == 0:
+                    prime = False
+                    break
+            if prime:
+                return value
+
+        raise IndexError("No prime number found! Try another length!")
+
+    def __hash_function(self, key, n):
+        """
+        Hash function using mod.
+        Args:
+            key: integer key to hash
+            n: mod n
+
+        Returns:
+            index of the key after hashing
+
+        """
+        return key % n
+
+    def __key_type_check(self, key):
+        """
+        Check the type of keys.
+
+        """
+        if self.key_type == "string":
+            if not isinstance(key, str):
+                raise ValueError("Key type error!")
+
+        elif self.key_type == "int":
+            if not isinstance(key, int):
+                raise ValueError("Key type error!")
+
+        else:
+            raise ValueError("Unknown key type!")
+
+    def __str_to_int(self, key: str):
+        """
+        Transform from string to integer, in order to use the hash function.
+
+        """
+        string = binascii.b2a_hex(key.encode('utf-8'))
+        return int(string, 16)
+
+    def insert(self, key, value):
+        """
+        Insert a key-value pair.
+        Args:
+            key: integer key
+            value: any value
+
+        """
+        self.__key_type_check(key)
+        key_int = self.__str_to_int(key) if self.key_type == "string" else key
+
+        index = self.__hash_function(key_int, self.hash_function_n)
+
+        if not self.hash_table[index].key:
+            self.hash_table[index].key = key
+            self.hash_table[index].value = value
+
+        # if the node has already existed, use a linear probing or double hashing
+        else:
+            pointer = index
+            node = self.hash_table[pointer]
+
+            if self.hash_mode == "LinearProbing":
+                step = 1
+            # double hashing
+            else:
+                step = self.__hash_function(key, self.hash_function_n2)
+
+            while self.hash_table[pointer].key:
+                pointer += step
+                if pointer >= self.length:
+                    pointer -= self.length
+                if pointer == index:
+                    raise IndexError("Hash table out of range!")
+
+            self.hash_table[pointer].key = key
+            self.hash_table[pointer].value = value
+
+    def search(self, key):
+        self.__key_type_check(key)
+        key_int = self.__str_to_int(key) if self.key_type == "string" else key
+
+        index = self.__hash_function(key_int, self.hash_function_n)
+
+        if not self.hash_table[index].key:
+            print("Key not found!")
+        else:
+            pointer = index
+            node = self.hash_table[pointer]
+
+            if self.hash_mode == "LinearProbing":
+                step = 1
+            # double hashing
+            else:
+                step = self.__hash_function(key, self.hash_function_n2)
+
+            while node.key:
+                pointer += step
+                if pointer >= self.length:
+                    pointer -= self.length
+                if self.hash_table[pointer].key == key:
+                    print("Key: {}, Value: {}".format(self.hash_table[pointer].key, self.hash_table[pointer].value))
+                    break
+                if pointer == index:
+                    print("Key not found!")
+                    break
+
+
 def main():
     #############################
     # hash table for integer
@@ -150,6 +291,35 @@ def main():
     hash_table_str.search('abc')
     hash_table_str.search('cdf')
     print('------------------------')
+
+    #############################
+    # hash table for linear probing
+    print("-> hash table for linear probing")
+
+    hash_table_linear_probing = HashTableUsingOpenAddressing(15)
+    hash_table_linear_probing.insert(3, 'abc')
+
+    # 16 % 13 = 3, so next index: 3 + 1 = 4
+    hash_table_linear_probing.insert(16, 'cdf')
+    hash_table_linear_probing.search(14)
+    hash_table_linear_probing.search(16)
+    print("index 3: {}".format(hash_table_linear_probing.hash_table[3].key))
+    print("index 4: {}".format(hash_table_linear_probing.hash_table[4].key))
+    print('------------------------')
+
+    #############################
+    # hash table for double hashing
+    print("-> hash table for double hashing")
+
+    hash_table_double_hashing = HashTableUsingOpenAddressing(15, mode='d')
+    hash_table_double_hashing.insert(4, 'abc')
+
+    # 17 % 13 = 4, and 17 % 5 = 2, so next index: 4 + 2 = 6
+    hash_table_double_hashing.insert(17, 'cdf')
+    hash_table_double_hashing.search(14)
+    hash_table_double_hashing.search(16)
+    print("index 3: {}".format(hash_table_double_hashing.hash_table[4].key))
+    print("index 6: {}".format(hash_table_double_hashing.hash_table[6].key))
 
 
 if __name__ == "__main__":
